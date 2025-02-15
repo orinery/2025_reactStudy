9f9a5f58a8ddd7f2895feb85631dedc0f0704a11
orinery <gucheahyun@gmail.com>
2025-02-15 18:01:14 +0900

    0215
    리액트 기초/ 환율 계산기

--- quiz/section05/src/App.jsx
+++ quiz/section05/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import OrderEditor from "./assets/components/OrderEditor";
+// import CurrentInput from "./assets/components/CurrentInput";
+import { useState } from "react";
 
 // function Welcome({ userName, isMember }) {
 //   return (
@@ -10,12 +12,51 @@ import OrderEditor from "./assets/components/OrderEditor";
 //     </h1>
 //   );
 // }
+function CurrentInput({ currency, value, onChange }) {
+  return (
+    <div>
+      <label>{currency}:</label>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => onChange(currency, e.target.value)}
+      />
+    </div>
+  );
+}
+const exchangeRate = 1400;
 
 function App() {
+  const [state, setState] = useState({
+    krw: 0,
+    usd: 0,
+  });
+
+  const onChange = (currency, value) => {
+    console.log({ currency, value });
+    if (currency === "krw") {
+      setState({
+        krw: value,
+        usd: value / exchangeRate,
+      });
+    } else {
+      setState({
+        krw: value * exchangeRate,
+        usd: value,
+      });
+    }
+  };
+
   return (
     <>
       {/* {<Welcome userName={"오리너리"} isMember={true} />} */}
-      <OrderEditor />
+      {/* <OrderEditor /> */}
+
+      <section>
+        <h1>환율 변환기 &#40;KRW - USD&#41;</h1>
+        <CurrentInput currency={"krw"} value={state.krw} onChange={onChange} />
+        <CurrentInput currency={"usd"} value={state.usd} onChange={onChange} />
+      </section>
     </>
   );
 }
